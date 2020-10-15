5a06f97ebf98608951f71da79a4aae244e2bc54b
starylass <52856524+starylass@users.noreply.github.com>
2020-10-15 22:51:30 +0200

    Update settings.py

--- demoVCI/settings.py
+++ demoVCI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +146,5 @@ EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'demovcimanage@gmail.com'
 EMAIL_HOST_PASSWORD = 'pomidor22'
+
+django_heroku.settings(locals())
